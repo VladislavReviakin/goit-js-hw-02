8f1caea782b922e7d11be5e8a53e8822204c3c72
tharkusha <vladreviakin@ukr.net>
2024-02-02 21:45:37 +0000

    fix 1

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function checkForSpam(message) {
-  return message.toLowerCase().includes(spam || sale);
+  return message.toLowerCase().includes("spam") || .includes("sale");
 }
 
 console.log(checkForSpam('Latest technology news')); // false
